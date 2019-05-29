7939fbf9d068d1f4c088831b2e5b8a36cb696155
GDVFox <gaaveer@gmail.com>
2019-05-29 15:56:34 +0300

    [ADD] CD master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,11 @@ chmod 600 ./2019_1_HotCode_id_rsa.pem
 ssh-keyscan -H 89.208.198.192 >> ~/.ssh/known_hosts
 for (( c=1; c<=$CONTAINERS_COUNT; c++ ))
 do
-    ssh -i ./2019_1_HotCode_id_rsa.pem ubuntu@89.208.198.192 docker stop warscript-users.$c || true && docker rm warscript-users.$c || true
+    ssh -i ./2019_1_HotCode_id_rsa.pem ubuntu@89.208.198.192 docker stop warscript-users.$c
+    if ssh -i ./2019_1_HotCode_id_rsa.pem ubuntu@89.208.198.192 test $? -eq 0
+    then
+        ssh -i ./2019_1_HotCode_id_rsa.pem ubuntu@89.208.198.192 docker rm warscript-users.$c || true
+    fi
     ssh -i ./2019_1_HotCode_id_rsa.pem ubuntu@89.208.198.192 docker run -e CONSUL_ADDR='$CONSUL_ADDR' \
                                                                     -e VAULT_ADDR='$VAULT_ADDR' \
                                                                     -e VAULT_TOKEN='$VAULT_TOKEN' \
